846a55da6fbbbcb5ad878e5f0113351d45b78d3d
Reza Mobaraki <rezam578@gmail.com>
2024-08-12 01:03:19 +0330

    fix local file

--- src/core/settings/django/local.py
+++ src/core/settings/django/local.py
@@ -2,11 +2,11 @@ from .base import *  # noqa
 
 INSTALLED_APPS += [  # noqa
     "django_extensions",
-    "debug_toolbar",
+    # "debug_toolbar",
 ]
 
 MIDDLEWARE += [  # noqa
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 INTERNAL_IPS = ["127.0.0.1", "localhost"]
