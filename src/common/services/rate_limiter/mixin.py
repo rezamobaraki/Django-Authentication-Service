04873b8c080db36a782077753da9a73aded0808c
Reza Mobaraki <rezam578@gmail.com>
2024-08-11 23:57:21 +0330

    Refactor(RateLimiter): remove unused files and add RateLimitMixin to AuthenticationViewSet

--- src/common/services/rate_limiter/mixin.py
+++ src/common/services/rate_limiter/mixin.py
@@ -1,4 +1,4 @@
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from rest_framework.exceptions import Throttled
 
 from common.services.rate_limiter.rate_limiter import RateLimiter
@@ -6,6 +6,7 @@ from common.services.rate_limiter.rate_limiter import RateLimiter
 
 class RateLimitMixin:
     rate_limiter_class = RateLimiter
+    message = _("You are now blocked due to too many failed attempts.")
 
     def check_user_block(self, request):
         identifiers = {
@@ -18,7 +19,7 @@ class RateLimitMixin:
         for identifier_value in identifiers.values():
             is_blocked, wait_time = rate_limiter.is_user_blocked(identifier_value)
             if is_blocked:
-                raise Throttled(detail=_("You are now blocked due to too many failed attempts."), wait=wait_time)
+                raise Throttled(detail=self.message, wait=wait_time)
 
     def handle_invalid_attempt(self, request):
         identifiers = {
@@ -30,7 +31,7 @@ class RateLimitMixin:
 
         for identifier_value in identifiers.values():
             if rate_limiter.increment_attempts(identifier_value):
-                raise Throttled(detail=_("You are now blocked due to too many failed attempts."))
+                raise Throttled(detail=self.message)
 
     def create(self, request, *args, **kwargs):
         self.check_user_block(request)
